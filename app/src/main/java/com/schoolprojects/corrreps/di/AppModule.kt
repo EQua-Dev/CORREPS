30cbbd497f542a62ddb280b8294a35c531a2da6c
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-06-25 04:46:18 +0100

    set up project with firebase and dependencies

---
+++ app/src/main/java/com/schoolprojects/corrreps/di/AppModule.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024.
+ * Luomy EQua
+ * Under Awesomeness Studios
+ */
+
+package awesomenessstudios.patientrecordanalysis.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
